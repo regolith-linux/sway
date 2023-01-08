395c0d2cc24923757da1867cdaa195756d2b6fbb
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2023-01-09 04:25:35 +0530

    Package changes

--- swaybar/tray/dbusmenu.c
+++ swaybar/tray/dbusmenu.c
@@ -545,7 +545,7 @@ static void swaybar_dbusmenu_draw_menu(struct swaybar_dbusmenu_menu *menu,
 		menu->dbusmenu->drawing = false;
 		return;
 	}
-	int surface_x, surface_y, surface_width, surface_height;
+	int surface_x = 0, surface_y = 0, surface_width = 0, surface_height = 0;
 	draw_menu_items(cairo, menu, &surface_x, &surface_y, &surface_width,
 					&surface_height, open);
 
