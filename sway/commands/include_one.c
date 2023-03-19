2801ed559659ad38b18cc6525e56e55e344a9471
DeepanshuPratik <deepanshu.pratik@gmail.com>
2023-03-19 17:50:37 +0530

    fixed memory management issues

--- sway/commands/include_one.c
+++ sway/commands/include_one.c
@@ -35,7 +35,9 @@ void priority_configs(const char *path, const char *parent_dir, struct sway_conf
 		for (i = 0; i < p.we_wordc; ++i) {
 			int index = list_seq_find(locations, (int (*)(const void *, const void *))compare_paths, w[i]);
 			char* matched_path = strdup(w[i]);
-			config_location_map config_loc = {basename(matched_path), matched_path};
+			config_location_map* config_loc = malloc(sizeof(config_location_map));
+			config_loc->name =  basename(matched_path);
+			config_loc->path = matched_path;
 			if (index == -1) {
 				list_add(locations, &config_loc);
 				continue;
@@ -51,6 +53,7 @@ void priority_configs(const char *path, const char *parent_dir, struct sway_conf
 	}
 cleanup:
 	free(wd);
+	list_free_items_and_destroy(locations);
 }
 
 struct cmd_results *cmd_include_one(int argc, char **argv) {
@@ -66,7 +69,6 @@ struct cmd_results *cmd_include_one(int argc, char **argv) {
 	
 	for(int i=0; i<argc ; ++i){
 		priority_configs(argv[i], parent_dir, config, locs);
-		sway_log(SWAY_ERROR, "PARSING INCLUDE PATH: %s", argv[i]);
 	}
 
 	config_location_map **locs_arr = (config_location_map**) locs->items;
